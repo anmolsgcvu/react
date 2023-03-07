dbba9d21bad807e24d394b6dbca38526b4bd3c87
anmolsgcvu <74961789+anmolsgcvu@users.noreply.github.com>
2023-03-07 18:32:49 +0530

    Updates

---
+++ precache-manifest.152cdc3b03d1b64b75149522e7eff504.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "814367fb5e8004269a17",
+    "url": "/anmolsgcvu/react/static/css/main.2ae38d68.chunk.css"
+  },
+  {
+    "revision": "814367fb5e8004269a17",
+    "url": "/anmolsgcvu/react/static/js/main.814367fb.chunk.js"
+  },
+  {
+    "revision": "927bd4bacd441db6510d",
+    "url": "/anmolsgcvu/react/static/js/1.927bd4ba.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/anmolsgcvu/react/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "8105dc185e89bda33168",
+    "url": "/anmolsgcvu/react/static/js/runtime~main.8105dc18.js"
+  },
+  {
+    "revision": "12321792aa58464270258d9270653890",
+    "url": "/anmolsgcvu/react/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/anmolsgcvu/react/precache-manifest.152cdc3b03d1b64b75149522e7eff504.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/anmolsgcvu/react/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
